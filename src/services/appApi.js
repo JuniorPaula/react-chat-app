6f5ea6a420b27d36d26f13ba8118917eaad4ce2e
Junior Paula <luke.junnior@icloud.com>
2022-09-03 16:52:32 -0300

    feat: create appApi service to map routes

---
+++ src/services/appApi.js
@@ -0,0 +1,35 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const appApi = createApi({
+    reducerPath: 'appApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: process.env.REACT_APP_SERVER_API_URL
+    }),
+    endpoints: (builder) => ({
+        signupUser: builder.mutation({
+            query: (user) => ({
+                url: '/users/create',
+                method: 'POST',
+                body: user
+            })
+        }),
+        loginUser: builder.mutation({
+            query: (user) => ({
+                url: '/users/login',
+                method: 'POST',
+                body: user
+            })
+        }),
+        logoutUser: builder.mutation({
+            query: (payload) => ({
+                url: '/users/logout',
+                method: 'DELETE',
+                body: payload
+            })
+        })
+    })
+});
+
+export const { useSignupUserMutation, useLoginUserMutation, useLogoutUserMutation} = appApi;
+
+export default appApi;
