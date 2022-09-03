d03aea8a77961d33c6f44ef07d9ff9a6dc255d68
Junior Paula <luke.junnior@icloud.com>
2022-09-03 15:23:41 -0300

    feat: create Messageform component

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import { Button, Col, Form, Row } from 'react-bootstrap'
+import './messageForm.css';
 
 export default function MessageForm() {
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+
   return (
-    <div>MessageForm</div>
+    <>
+        <div className="message-output"></div>
+        <Form onSubmit={handleSubmit}>
+            <Row>
+                <Col md={11}>
+                    <Form.Group>
+                        <Form.Control 
+                            type="text"
+                            placeholder="Digite sua mensagem">                             
+                        </Form.Control>
+                    </Form.Group>
+                </Col>
+                <Col md={1}>
+                    <Button 
+                        variant="primary" 
+                        type="submit" 
+                        style={{ width: "100%", backgoundColor: 'orange'}}>
+                            <i className="fas fa-paper-plane"></i>
+                    </Button>
+                </Col>
+            </Row>
+        </Form>
+    </>
+    
   )
 }
