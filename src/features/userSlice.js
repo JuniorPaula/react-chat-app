561ac4977d7fe44d4c8ab3be21f887e2ff51392e
Junior Paula <luke.junnior@icloud.com>
2022-09-03 17:08:05 -0300

    feat: create userSlcie

---
+++ src/features/userSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+import appApi from '../services/appApi';
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState: null,
+    reducers: {
+        addNotifications: (state, { payload }) => {},
+        resetNotifications: (state, { payload }) => {}
+    },
+
+    extraReducers: (builder) => {
+        builder.addMatcher(appApi.endpoints.signupUser.matchFulfilled, (state, { payload }) => payload);
+        builder.addMatcher(appApi.endpoints.loginUser.matchFulfilled, (state, { payload }) => payload);
+        builder.addMatcher(appApi.endpoints.logoutUser.matchFulfilled, () => null);
+    }
+})
+
+export const { addNotifications, resetNotifications } = userSlice.actions;
+
+export default userSlice.reducer;
