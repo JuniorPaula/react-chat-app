655384203f0b8faeac08cee00cb5df59aa22ddc1
Junior Paula <luke.junnior@icloud.com>
2022-09-04 11:35:17 -0300

    feat: get message to the server

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import { Button, Col, Form, Row } from 'react-bootstrap'
 import { useSelector } from 'react-redux';
+import { appContext } from '../context/appContext';
 import './messageForm.css';
 
 export default function MessageForm() {
     const user = useSelector((state) => state.user);
+    const { socket, currentRooms, messages, setMessages, privateMembersMsg} = useContext(appContext)
+    const [message, setMessage] = useState('');
+
+    function getFormatedDate() {
+        const date = new Date();
+        const year = date.getFullYear();
+        let month = (1 + date.getMonth()).toString();
+
+        month = month.length > 1 ? month : '0' + month;
+
+        let day = date.getDay().toString();
+
+        day = day.length > 1 ? day : '0' + day;
+
+        return `${day}/${month}/${year}`;
+    }
+
+    const todaydate = getFormatedDate();
+
+    socket.off('room-messages').on('room-messages', (roomMessages) => {
+        console.log('room messages', roomMessages);
+        setMessage(roomMessages);
+    });
+
     function handleSubmit(e) {
         e.preventDefault();
+        const today = new Date();
+        const minutes = today.getMinutes() < 10 ? '0' + today.getMinutes() : today.getMinutes();
+        const time = today.getHours() + ':' + minutes;
+        const roomId = currentRooms;
+        socket.emit('message-room', roomId, message, user, time, todaydate);
+        setMessage('');
     }
 
   return (
@@ -21,6 +52,8 @@ export default function MessageForm() {
                         <Form.Control 
                             type="text"
                             disabled={!user}
+                            value={message}
+                            onChange={e => setMessage(e.target.value)}
                             placeholder="Digite sua mensagem">                             
                         </Form.Control>
                     </Form.Group>
